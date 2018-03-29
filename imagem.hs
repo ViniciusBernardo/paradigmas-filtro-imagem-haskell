c4f836dc4ada2eea447aa108090bc24ca110875b
Vinicius Ferreira <vfbdelima@gmail.com>
2018-03-29 00:26:27 -0300

    Define function to mount data structure of image
    Signed-off-by: Vinicius Ferreira <vfbdelima@gmail.com>

--- imagem.hs
+++ imagem.hs
@@ -24,3 +24,17 @@ somaLista (h:t) = unwords([show(15 - (read h :: Int))]) ++ " " ++ somaLista(t)
 addSpace xs width = if length xs <= width
                     then xs 
                     else take width xs ++ "\n" ++ addSpace (drop width xs) width
+
+mountImage :: [String] -> Int -> Int -> [Pixel] -> [[Pixel]] -> [[Pixel]]
+mountImage [] _ _ _ a = a
+mountImage xs counter width listPixels image = do
+    let slicedList = take 3 xs
+    if counter < width
+    then do
+        [listPixels] ++ [[Pixel (read (slicedList!!0) :: Int) (read (slicedList!!1) :: Int) (read (slicedList!!2) :: Int)]]
+        drop 3 xs
+        mountImage xs (counter+1) width listPixels image
+    else do
+        image ++ [listPixels]
+        listPixels <- []
+        mountImage xs 0 width listPixels image
