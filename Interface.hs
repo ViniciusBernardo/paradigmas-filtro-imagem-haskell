d2b1f4c01c99c49d064cbad8011076de0f02badb
Vinicius Ferreira <vfbdelima@gmail.com>
2018-04-05 20:50:37 -0300

    Improve usability of select filter
    Signed-off-by: Vinicius Ferreira <vfbdelima@gmail.com>

--- Interface.hs
+++ Interface.hs
@@ -3,26 +3,31 @@ import Filtros (filtroNegativo, filtroPolarizado, filtroPretoEBranco)
 import ReadAndSavePPM
 import System.IO
 
-menu = do
+menu _ True = do
     putStrLn "Insira o nome o arquivo (formato .ppm)"
     filePath <- getLine
     f <- openBinaryFile filePath ReadMode
     image <- leArquivo f 0 []
+    menu image False
+menu image False = do
     putStrLn "------------------------Menu de Operações--------------------------"
     putStrLn "- (1) Negativo                               (2) Polarizado       -"
     putStrLn "- (3) Preto e Branco                         (4) Sair             -"
     putStrLn "-------------------------------------------------------------------"
     putStrLn "Escolha uma opção"
     option <- getLine
-    let imageFiltered = if option == "1"
-                            then filtroNegativo image
-                            else if option == "2"
-                                then filtroPolarizado image
-                                else if option == "3"
-                                then filtroPretoEBranco image
-                                else [[]]
-    salvaArquivo imageFiltered
-    putStrLn "Imagem salve com sucesso!"
+    if option == "1"
+        then (menu (filtroNegativo image) False)
+        else if option == "2"
+        then (menu (filtroPolarizado image) False)
+        else if option == "3"
+        then (menu (filtroPretoEBranco image) False)
+        else if option == "4"
+        then salvaArquivo image
+        else do
+            putStrLn "Escolha uma opção válida!\n"
+            (menu image False)
+    putStrLn "Imagem salva com sucesso!"
 
 salvaArquivo imagem = do
     putStrLn "Insira o nome o arquivo (formato .ppm)"
